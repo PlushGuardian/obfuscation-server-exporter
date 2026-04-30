c889d49239f1b8fa12353c3862a8916dfe01b4d1
Elizaveta Bolshova <bolshovaeliza@gmail.com>
2026-04-30 21:48:50 +0300

    fix errors

--- config/config.go
+++ config/config.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net"
 	"net/url"
-	"strings"
 )
 
 type Config struct {
--- threexui/client.go
+++ threexui/client.go
@@ -69,9 +69,17 @@ func (c *threeXUIClient) getAuthToken() (*http.Cookie, error) {
 		"username": {c.config.Username},
 		"password": {c.config.Password},
 	}
+
+	u, err := c.config.PanelURL()
+	if err != nil {
+		c.logger.Fatalf("failed to build 3x-ui panel URL: %v", err)
+		return nil, err
+	}
+
 	req, err := http.NewRequest(http.MethodPost,
-		c.config.PanelURL()+"/login",
+		u+"/login",
 		strings.NewReader(data.Encode()))
+
 	if err != nil {
 		c.logger.Printf("3x-ui: create login request failed: %v", err)
 		return nil, err
@@ -116,7 +124,13 @@ func (c *threeXUIClient) getAuthToken() (*http.Cookie, error) {
 }
 
 func (c *threeXUIClient) do(method, path string, cookie *http.Cookie) ([]byte, error) {
-	req, err := http.NewRequest(method, c.config.PanelURL()+path, nil)
+	u, err := c.config.PanelURL()
+	if err != nil {
+		c.logger.Fatalf("failed to build 3x-ui panel URL: %v", err)
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, u+path, nil)
 	if err != nil {
 		c.logger.Printf("3x-ui: create request %s %s failed: %v", method, path, err)
 		return nil, err
