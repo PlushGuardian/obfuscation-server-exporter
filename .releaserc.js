932713265b38325a8e9d3a3d66d2a81c451b9360
Elizaveta Bolshova <bolshovaeliza@gmail.com>
2026-04-30 14:29:20 +0300

    use another lightning

--- .releaserc.js
+++ .releaserc.js
@@ -18,7 +18,7 @@ module.exports = {
             refactor: '↻ Refactoring',
             test: '⚑ Tests',
             build: '⚙ Build',
-            ci: '⌁ CI',
+            ci: '⊶ CI',
             chore: '☑ Chores'
           };
 
