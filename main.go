b2f3f188b6e44e81e92b7821e824cbdedf8291e6
Elizaveta Bolshova <bolshovaeliza@gmail.com>
2026-05-05 12:57:42 +0300

    add all flags

--- main.go
+++ main.go
@@ -22,13 +22,13 @@ func main() {
 	// threeXUILogger := log.New(file, "[3x-ui] ", log.LstdFlags)
 	// mtproxyMaxLogger := log.New(file, "[mtproxymax] ", log.LstdFlags)
 
-	// --- Create main function ------------------------------
+	// --- Create flags -------------------------------------
 
 	v := viper.New()
 	fs := pflag.NewFlagSet(os.Args[0], pflag.ExitOnError)
 	fs.SortFlags = false
 	err = initializeFlags(v, fs)
-	if initializeFlags(v, fs) != nil {
+	if err != nil {
 		obfsExporterLogger.Fatal(err.Error())
 	}
 
@@ -38,16 +38,32 @@ func main() {
 
 func initializeFlags(v *viper.Viper, fs *pflag.FlagSet) error {
 	fs.String("config-file", "", "Path to YAML configuration file")
-	fs.Int("metrics-port", 9100, "Port for the metrics exporter")
+
+	fs.Int("metrics-port", 9100, "Port for the obfuscation-server-exporter to listen on")
+	fs.String("metrics-path", "/metrics", "Path the obfuscation-server-exporter listens on")
+	fs.Int("scrape-timeout", 30, "Scrape timeout for the metrics port of the obfuscation-server-exporter")
+
 	fs.Int("threexui-panel-port", 2053, "3X‑UI panel port")
+	fs.String("threexui-panel-path", "", "3X‑UI panel path")
+	fs.String("threexui-panel-username", "", "3X‑UI username")
+	fs.String("threexui-panel-password", "", "3X‑UI password")
+	fs.Bool("threexui-insecure-skip-verify", false, "Skip TLS verification")
+	fs.Int("threexui-clients-bytes-rows", 0, "Top N rows for client bytes")
+	fs.Int("threexui-timeout", 15, "Request timeout for the 3x-ui panel")
+
+	fs.Int("mtproxymax-metrics-port", 9090, "3X‑UI panel port")
+	fs.String("mtproxymax-metrics-path", "/metrics", "MTProxyMax metrics path")
 
 	if err := v.BindPFlags(fs); err != nil {
 		return fmt.Errorf("error when binding flags: %w", err)
 	}
 
 	rules := map[string]string{
-		"^metrics-port$": "obfs-exporter.metrics-port",
-		"^threexui-":     "threexui.",
+		"^metrics-port$":   "obfs-exporter.metrics-port",
+		"^metrics-path$":   "obfs-exporter.metrics-path",
+		"^scrape-timeout$": "obfs-exporter.scrape-timeout",
+		"^threexui-":       "threexui.",
+		"^mtproxymax-":     "mtproxymax.",
 	}
 
 	if err := config.AliasFlags(v, fs, rules); err != nil {
